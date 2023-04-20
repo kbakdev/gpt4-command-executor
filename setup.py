63750d85cda8303c7a45193828f2a55485271719
Kacper Bąk <contact@kacperbak.pl>
2023-04-20 22:38:18 +0200

    feat: update on setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="gpt4-command-executor",
     version="0.1",
+    author="Kacper Bąk",
+    author_email="contact@kacperbak.pl",
+    description="A package that uses GPT-4 to execute commands",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
