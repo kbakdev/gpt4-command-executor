bf0c2247e949f48d891abec3503f0863a9bf8335
Bąk Kacper <contact@kacperbak.pl>
2023-04-21 11:41:47 +0200

    feat: upgrade engine of gpt3

--- gpt4_command_executor/gpt3.py
+++ gpt4_command_executor/gpt3.py
@@ -32,7 +32,7 @@ def generate_command(query):
         max_tokens=50,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.9,
     )
 
     command = response.choices[0].text.strip()
@@ -41,12 +41,12 @@ def generate_command(query):
 
 def revise_command(command):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=f"Please revise the following shell command to improve its accuracy: {command}",
         max_tokens=50,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.9,
     )
 
     revised_command = response.choices[0].text.strip()
@@ -55,12 +55,12 @@ def revise_command(command):
 
 def generate_explanation(command):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=f"Explain the following shell command in simple terms: {command}",
         max_tokens=100,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.9,
     )
 
     explanation = response.choices[0].text.strip()
